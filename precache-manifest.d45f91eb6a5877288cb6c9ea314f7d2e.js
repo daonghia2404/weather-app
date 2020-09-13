f0b5f2503acf652dfc7122899a98b696cab60bde
daonghia2404 <daonghia2404@gmail.com>
2020-09-13 18:28:00 +0700

    Updates

---
+++ precache-manifest.d45f91eb6a5877288cb6c9ea314f7d2e.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d278e382024fb2534e047bd414075d1a",
+    "url": "/daonghia2404/weather-app/index.html"
+  },
+  {
+    "revision": "e7cdbeeede28cca28e2e",
+    "url": "/daonghia2404/weather-app/static/css/2.34c0de18.chunk.css"
+  },
+  {
+    "revision": "88b5ca5bc5654169dfa2",
+    "url": "/daonghia2404/weather-app/static/css/main.4b70b48c.chunk.css"
+  },
+  {
+    "revision": "e7cdbeeede28cca28e2e",
+    "url": "/daonghia2404/weather-app/static/js/2.aef5ac92.chunk.js"
+  },
+  {
+    "revision": "241e2720c0228744c712295a835ee806",
+    "url": "/daonghia2404/weather-app/static/js/2.aef5ac92.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "88b5ca5bc5654169dfa2",
+    "url": "/daonghia2404/weather-app/static/js/main.79866f46.chunk.js"
+  },
+  {
+    "revision": "1a5cd0e11be226589416",
+    "url": "/daonghia2404/weather-app/static/js/runtime-main.6b15506c.js"
+  },
+  {
+    "revision": "608f708a04a268d12e5832d35eb68476",
+    "url": "/daonghia2404/weather-app/static/media/cloud-black.608f708a.png"
+  },
+  {
+    "revision": "2466f2ce2615957a08a7062794571255",
+    "url": "/daonghia2404/weather-app/static/media/cloud-white.2466f2ce.png"
+  },
+  {
+    "revision": "c0a9e633069e9e5821b0b47922ec1727",
+    "url": "/daonghia2404/weather-app/static/media/icon-almosphere.c0a9e633.png"
+  },
+  {
+    "revision": "b22912b60ec86b2ba54f11171a31dd58",
+    "url": "/daonghia2404/weather-app/static/media/icon-cloud-lightning.b22912b6.png"
+  },
+  {
+    "revision": "ea6fbc5aa8dfaf3b23b44907be8c8737",
+    "url": "/daonghia2404/weather-app/static/media/icon-cloud-rain.ea6fbc5a.png"
+  },
+  {
+    "revision": "d1d1caf292b3dfaf5429b7fdb1f7c812",
+    "url": "/daonghia2404/weather-app/static/media/icon-cloud-snows.d1d1caf2.png"
+  },
+  {
+    "revision": "472b6589b2f5f577272d92fd0a4e03ec",
+    "url": "/daonghia2404/weather-app/static/media/icon-clouds.472b6589.png"
+  },
+  {
+    "revision": "e9f28984b474b14f0fc5d8f9df27de06",
+    "url": "/daonghia2404/weather-app/static/media/icon-snow.e9f28984.png"
+  },
+  {
+    "revision": "166776cf27ddc46178e13c74976ae23a",
+    "url": "/daonghia2404/weather-app/static/media/icon-sun-cloud-rains.166776cf.png"
+  },
+  {
+    "revision": "759632b864a904cf61ae50a29b1e5a59",
+    "url": "/daonghia2404/weather-app/static/media/icon-sun-cloud.759632b8.png"
+  },
+  {
+    "revision": "21b78d09e09a43ad0ff03bce8b88a217",
+    "url": "/daonghia2404/weather-app/static/media/icon-sun.21b78d09.png"
+  },
+  {
+    "revision": "40cf9ea074a57d3441ea485ff26c981e",
+    "url": "/daonghia2404/weather-app/static/media/icon-sun.40cf9ea0.png"
+  },
+  {
+    "revision": "6a862b8575145248ee7b83ad78e40179",
+    "url": "/daonghia2404/weather-app/static/media/moon.6a862b85.png"
+  },
+  {
+    "revision": "ab1710bda327e07791cba155787bddab",
+    "url": "/daonghia2404/weather-app/static/media/sun.ab1710bd.png"
+  }
+]);
